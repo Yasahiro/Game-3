d2e96d9003c2511ff8e7a222213a297c66b726e9
Yasahiro <yasahiro93@gmail.com>
2013-03-10 14:47:09 +0000

    Update

--- src/uk/co/marshmallow_zombies/Game3/Game3.java
+++ src/uk/co/marshmallow_zombies/Game3/Game3.java
@@ -4,11 +4,14 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import uk.co.marshmallow_zombies.rj2dgl.framework.Game;
+import uk.co.marshmallow_zombies.rj2dgl.tilesets.Map;
 
 public class Game3 extends Game {
 
 	public static final int GAME_WIDTH = 25 * 32;
 	public static final int GAME_HEIGHT = 19 * 32;
+
+	private Map map;
 
 	public Game3() {
 
@@ -17,8 +20,9 @@ public class Game3 extends Game {
 	@Override
 	protected void init() {
 		System.out.println("init()");
-		// TODO init code here
 		
+		map = Map.load("res/levels/level1");
+
 		super.init();
 		screen.setSize(GAME_WIDTH, GAME_HEIGHT);
 	}
@@ -26,30 +30,30 @@ public class Game3 extends Game {
 	@Override
 	protected void start() {
 		System.out.println("start()");
-		
+
 		super.start();
 	}
 
 	@Override
 	protected void stop() {
 		System.out.println("stop()");
-		
+
 		super.stop();
 	}
-	
+
 	@Override
 	protected void tick(long delta) {
 		// TODO update logic
-		
+
 		super.tick(delta);
 	}
 
 	@Override
-	protected void render(Graphics g, long delta) {		
+	protected void render(Graphics g, long delta) {
 		screen.clear(Color.BLUE);
-		
-		// TODO drawing code
-		
+
+		drawMap();
+
 		super.render(g, delta);
 	}
 
