3b1dea7a4524f51af4b5e40cf342d6589d52ce73
Yasahiro <yasahiro93@gmail.com>
2013-03-11 11:42:02 +0000

    Map reading

--- src/uk/co/marshmallow_zombies/Game3/Game3.java
+++ src/uk/co/marshmallow_zombies/Game3/Game3.java
@@ -1,15 +1,21 @@
 package uk.co.marshmallow_zombies.Game3;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import uk.co.marshmallow_zombies.rj2dgl.framework.Game;
+import uk.co.marshmallow_zombies.rj2dgl.tilesets.Layer;
 import uk.co.marshmallow_zombies.rj2dgl.tilesets.Map;
+import uk.co.marshmallow_zombies.rj2dgl.tilesets.Tile;
+import uk.co.marshmallow_zombies.rj2dgl.tilesets.TileLayer;
 
 public class Game3 extends Game {
-
-	public static final int GAME_WIDTH = 25 * 32;
-	public static final int GAME_HEIGHT = 19 * 32;
 
 	private Map map;
 
@@ -19,25 +25,21 @@ public class Game3 extends Game {
 
 	@Override
 	protected void init() {
-		System.out.println("init()");
-		
 		map = Map.load("res/levels/level1");
 
+		window.setTitle(String.format("%d x %d", map.getWidth(), map.getHeight()));
+
 		super.init();
-		screen.setSize(GAME_WIDTH, GAME_HEIGHT);
+		screen.setSize(map.getWidth() * map.getTileWidth(), map.getHeight() * map.getTileHeight());
 	}
 
 	@Override
 	protected void start() {
-		System.out.println("start()");
-
 		super.start();
 	}
 
 	@Override
 	protected void stop() {
-		System.out.println("stop()");
-
 		super.stop();
 	}
 
@@ -50,11 +52,45 @@ public class Game3 extends Game {
 
 	@Override
 	protected void render(Graphics g, long delta) {
-		screen.clear(Color.BLUE);
+		screen.clear(map.getBackgroundColor());
 
-		drawMap();
+		drawMap(g);
 
 		super.render(g, delta);
 	}
 
+	private void drawMap(Graphics g) {
+		Layer[] layers = map.getLayers();
+
+		for (int j = 0; j < layers.length; j++) {
+			Layer layer = layers[j];
+
+			if (layer instanceof TileLayer) {
+				Tile[] tiles = ((TileLayer) layer).getTiles();
+				int w = tiles.length;
+
+				for (int i = 0; i < w; i++) {
+					if (tiles[i].equals(Tile.EMPTY))
+						continue;
+
+					int x = i % map.getWidth() * map.getTileWidth();
+					int y = i / map.getWidth() * map.getTileHeight();
+					BufferedImage image = tiles[i].getImage();
+					File file = new File("/home/oliver/Pictures/test/l" + j + "_t" + i);
+					try {
+						if (!file.exists())
+							ImageIO.write(image, "png", file);
+					} catch (IOException e) {
+					}
+
+					g.drawImage(image, x, y, null);
+
+					g.setFont(new Font("Arial", Font.PLAIN, 10));
+					g.setColor(Color.BLUE);
+					g.drawString(String.format("%d", j + 1), x, y + 16);
+				}
+				System.out.printf("Layer %d\n", j + 1);
+			}
+		}
+	}
 };
